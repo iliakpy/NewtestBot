0013dfb357d235395a957ca9b7981284e489ea69
iliak2 <iliak2@gmail.com>
2021-12-01 14:26:38 +0300

    Bot Update and features

---
+++ chat bot 2.py
@@ -0,0 +1,21 @@
+def user_input():
+    user_line = input('Введите Текст:  ')
+    return(user_line)
+
+def reply(text):
+    print('Чего изволит хозяин:', text)
+
+
+def handle_command(command):
+    command = command.lower()
+    if command == 'привет':
+        reply('Привет-Привет!')
+    elif command == 'пока':
+        reply('Пока!')
+    elif command == 'закажи еду':
+        reply('Пиццу или борщ?')
+    else:
+        reply('Не понятно, повторите')
+
+user_text = user_input()\
+handle_command(user_text)
